bbfd48f2f65794afa1ddaa09997f2a0da762458f
Maximiliano Hernández <max.hernandezg@duocuc.cl>
2024-06-24 20:00:46 -0400

    Se agrega Templates

--- bikes/views.py
+++ bikes/views.py
@@ -1,3 +1,18 @@
 from django.shortcuts import render
 
 # Create your views here.
+def index(request):
+    return render(request, 'bikes/index.html')
+
+def contact(request):
+    return render(request, 'bikes/contact.html')
+
+def about(request):
+    return render(request, 'bikes/about.html')
+
+def login(request):
+    return render(request, 'bikes/login.html')
+
+def shopsingle(request):
+    return render(request, 'bikes/shop-single.html')
+
